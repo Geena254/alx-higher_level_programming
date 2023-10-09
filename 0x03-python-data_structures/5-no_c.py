452053135146cd72f1b41127b53b8c9cfaaf40d6
Georgina Kimani <njokikimani001@gmail.com>
2023-10-09 16:17:47 -0700

    No c C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!usr/bin/python3
 def no_c(my_string):
+    if not my_string:  # Check if the input string is empty
+        return my_string  # Return the empty string as-is
     new_str = ' '
     for x in my_string:
         if x != 'c' and x != 'C':
-            new_str += 1
+            new_str += x
     return (new_str)

3ad8324ef3b111473a395f817c2de2456e66dc92
Georgina Kimani <njokikimani001@gmail.com>
2023-10-08 18:20:58 -0700

    Lists and Tuples

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <object.h>
+#include <listobject.h>
+
+/**
+ * print_python_list_info - Function that prints some basic info about python list
+ * @p: the python object
+**/
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int a = 0;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	while (a < size)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+		a++;
+	}
+}
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+def no_c(my_string):
+    new_str = ' '
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_str += 1
+    return (new_str)

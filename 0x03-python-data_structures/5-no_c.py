5a9233a3dcab7c21d3bfc21ee4880ef34673e6e6
Georgina Kimani <njokikimani001@gmail.com>
2023-10-09 16:43:28 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,9 @@
 def no_c(my_string):
     if not my_string:  # Check if the input string is empty
         return my_string  # Return the empty string as-is
-    new_str = ' '
+    new_str_list = []
     for x in my_string:
         if x != 'c' and x != 'C':
-            new_str += x
+            new_str_list.append(x)  # Append characters to the list
+    new_str = ''.join(new_str_list)  # Join the list into a single string
     return (new_str)

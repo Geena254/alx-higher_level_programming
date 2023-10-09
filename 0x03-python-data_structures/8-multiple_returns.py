7ef501aa13a9e0afedb85739da65df2db54f7f5d
Georgina Kimani <njokikimani001@gmail.com>
2023-10-09 14:52:59 -0700

    multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence != ''
+    if sentence != '':
         first_char = sentence[0]
     else:
         first_char = None

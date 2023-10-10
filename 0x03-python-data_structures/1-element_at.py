1a885474e64537b3265abda7b77285e225737fcc
Georgina Kimani <njokikimani001@gmail.com>
2023-10-09 17:15:30 -0700

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if my_list == 0:
-        return
+    if not my_list:  # Check if the list is empty
+        return None
     elif idx < 0 or idx >= len(my_list):
-        return ("None")
+        return None
     else:
         return my_list[idx]

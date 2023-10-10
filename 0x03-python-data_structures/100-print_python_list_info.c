f4686feb031d0d270ff96831d3a914f47f4446fd
Georgina Kimani <njokikimani001@gmail.com>
2023-10-10 06:25:07 -0700

    python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -18,6 +18,6 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (a = 0; a < size; a++)
 	{
-		printf("Element %i: %s\n", a, Py_TYPE(obj->ob_item[i])->tp_name);
+		printf("Element %i: %s\n", a, Py_TYPE(obj->ob_item[a])->tp_name);
 	}
 }

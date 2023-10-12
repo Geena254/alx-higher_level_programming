902366239152b31d34ea85cef533cc556817c3e3
Georgina Kimani <njokikimani001@gmail.com>
2023-10-11 17:41:25 -0700

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return None
-    new_list = my_list[:]  #create a copy of the list
+    new_list = my_list[:]  # create a copy of the list
     if search in new_list:
         for i in range(len(new_list)):
             if new_list[i] == search:

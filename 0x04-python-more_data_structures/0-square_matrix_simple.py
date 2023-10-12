37e3447e45feda293c9e457135c8865108a47687
Georgina Kimani <njokikimani001@gmail.com>
2023-10-11 17:40:04 -0700

    Update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,13 +3,14 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return []
-    # Create a new matrix with the same size as the input matrix.
+    # Create a new matrix with the same size as the input matrix
     new_matrix = []
 
     for row in matrix:
-        new_row = []  # Create a new row for the new matrix.
+        new_row = []  # Create a new row for the new matrix
         for value in row:
-            new_row.append(value * value)  # Compute the square of each value and add it to the new row.
-        new_matrix.append(new_row)  # Add the new row to the new matrix.
+            # Compute the square of each val, add it to the new row
+            new_row.append(value * value)
+        new_matrix.append(new_row)  # Add the new row to the new matrix
 
     return new_matrix

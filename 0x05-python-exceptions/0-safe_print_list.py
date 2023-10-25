1f4d889d8a1fe93f361a0407075dea7acd33253a
Georgina Kimani <njokikimani001@gmail.com>
2023-10-25 07:17:30 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:  # Check if my_list is empty
-        return 0
-
     printed_count = 0
 
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-            printed_count += 1
-    except (IndexError, TypeError):
-        pass  # Catch exceptions for out-of-bounds index or type errors
+    if my_list is not None:
+        try:
+            for i in range(x):
+                print(my_list[i], end="")
+                printed_count += 1
+        except (IndexError, TypeError):
+            pass  # Catch exceptions for out-of-bounds index or type errors
 
     print()  # Print a new line after all elements
 

fdc869718513afc2893d0cb7e6f70493906ba70a
Georgina Kimani <njokikimani001@gmail.com>
2023-10-25 05:20:03 -0700

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -26,5 +26,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError as e:
             print(e)
             result.append(0)
-
-    return result
+        finally:
+            return result

1a25e00d014aada348d7f0f5edca3c25283ad821
Georgina Kimani <njokikimani001@gmail.com>
2023-11-18 22:29:04 -0800

    validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """Represent a rectangle."""
 
-    #class constructor
+    # class constructor
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
         Args:

11f318e8aa75a199e0f8a7593190a9eccee52e47
Georgina Kimani <njokikimani001@gmail.com>
2023-11-18 23:33:25 -0800

    dict square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,3 +49,13 @@ class Square(Rectangle):
         for key in kwargs:
             if key in atr_list:
                 setattr(self, key, kwargs[key])
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the Square Class"""
+        res = {
+                'id': self.id,
+                'x': self.x,
+                'size': self.size,
+                'y': self.y
+                }
+        return res
